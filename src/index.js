d6d1d04f5183bab7354f1ab53be0238e384d3161
John Lanni <82123623+JohnLanni619@users.noreply.github.com>
2021-12-04 13:11:37 -0500

    updated server for production

--- src/index.js
+++ src/index.js
@@ -14,12 +14,15 @@ async function startApolloServer(typeDefs, resolvers) {
     },
   });
 
-  const { url, port } = await server.listen();
-  console.log(`
-      🚀  Server is running
-      🔉  Listening on port ${port}
-      📭  Query at ${url}
-    `);
+  const { url, port } = await server.listen({port: process.env.PORT || 4000});
+    if (port===4000) {
+      console.log(`
+        🚀  Server is running
+        🔉  Listening on port ${port}
+        📭  Query at ${url}
+      `);
+  }
+  
 }
 
 startApolloServer(typeDefs, resolvers);
